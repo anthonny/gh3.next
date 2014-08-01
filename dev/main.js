1dd62c1f08192d8ad1904ccd2ebd14bbccc1d98a
k33g <ph.charriere@gmail.com>
2014-08-01 08:44:55 +0200

    gardening

---
+++ dev/main.js
@@ -0,0 +1,36 @@
+import gh3 from './src/gh3';
+
+console.log("start")
+
+window.k33g = new gh3.User({login:"k33g"});
+k33g.fetch().then((data) => {
+  console.log(k33g);
+}).catch((error) => {
+  console.log(error);
+});
+
+window.githubUsers = new gh3.Users();
+
+githubUsers.search("mad").then((data)=>{
+  console.log(githubUsers)
+}).catch((error) => {
+  console.log(error);
+});
+
+
+window.gh3Repo = new gh3.Repository({
+  owner: new gh3.User({
+    login:"k33g"
+  }),
+  name:"gh3"
+});
+
+gh3Repo.fetch().then((data) => {
+  console.log(gh3Repo);
+}).catch((error) => {
+  console.log(error);
+});
+
+
+
+
---
+++ dev/src/User.js
@@ -0,0 +1,31 @@
+//https://developer.github.com/v3/users/
+
+import r2 from './dependencies/r2/r2';
+
+class User   {
+  constructor (properties) {
+    Object.assign(this, properties);
+    this.url = "https://api.github.com/users/" + this.login;
+  }
+
+  fetch () {
+    return new r2.Request(this.url).jsonp().then((properties) => { Object.assign(this, properties.data); })
+  }
+
+  fetchRepositories () {
+    //TODO
+    //https://developer.github.com/v3/repos/#list-user-repositories
+    //create repository Class
+  }
+}
+
+export default User;
+
+/*
+window.k33g = new User({login:"k33g"});
+k33g.fetch().then((data) => {
+  console.log(k33g)
+}).catch((error) => {
+  console.log(error);
+});
+ */
